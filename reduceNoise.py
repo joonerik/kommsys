6b785f6c5ca02d2095560e07802e5a8505ad3d1f
Sindre Solheim <sindsolh@stud.ntnu.no>
2021-04-14 17:38:06 +0000

    typo

--- reduceNoise.py
+++ reduceNoise.py
@@ -3,11 +3,11 @@ import numpy as np
 from scipy.io import wavfile
 
 def reduce_noise(filepath, reduction_strength=0.5, volume=1.0):
-    """Read audio data from a wav file, reduce noise and save the new audi clip to noise_reduce.wav.
+    """Read audio data from a wav file, reduce noise and save the new audi clip to noise_reduce.wav
 
     Args:
         filepath (str): Relative path to a wavfile containing the noisy clip.
-        reduction_strength (float): A value between 0 and 1 describing ti what extend the noise should be reduced.
+        reduction_strength (float): A value between 0 and 1 describing to what extend the noise should be reduced.
         volume(float): A value to increase the volume of the output file. Often usefull in combination with hard noise reductions.
     """
     # Load the data
