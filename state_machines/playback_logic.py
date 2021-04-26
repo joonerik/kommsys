dd6ba86e521f34e90e137c3c11735f672099d366
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-04-26 19:05:29 +0200

    cleanup: remove a lot on stm as there is no playing state anymore

--- state_machines/playback_logic.py
+++ state_machines/playback_logic.py
@@ -38,7 +38,6 @@ class Player:
             self.client.disconnect()
 
     def on_message(self, client, userdata, msg):
-        print("on_message(): topic: {}".format(msg.topic))
         if not self.emg_mode:
             try:
                 data = json.loads(msg.payload)
@@ -51,41 +50,6 @@ class Player:
                 print("ValueError raised !!!")
                 pass
         
-    def play(self):
-        if not self.emg_mode:
-            # Set chunk size of 1024 samples per data frame
-            chunk = 1024  
-
-            # Open the sound file 
-            wf = wave.open(self.filename, 'rb')
-
-            # Create an interface to PortAudio
-            p = pyaudio.PyAudio()
-
-            # Open a .Stream object to write the WAV file to
-            # 'output = True' indicates that the sound will be played rather than recorded
-            stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                            channels = wf.getnchannels(),
-                            rate = wf.getframerate(),
-                            output = True)
-
-            # Read data in chunks
-            data = wf.readframes(chunk)
-            # Play the sound by writing the audio data to the stream
-            while data != (b'') and not self.emg_mode:
-                stream.write(data)
-                data = wf.readframes(chunk)
-
-            # Close and terminate the stream
-            print("Message is finished")
-            stream.close()
-            p.terminate()
-        else:
-            # TODO: Test if an incoming message on a channel subscribed is ignored if a emg msg is playing
-            # Remember to argue for this in the systemSpec. It is also not likely that a person is
-            # sending a msg while an emg msg is played
-            print("Emergency mode ON - can't play incoming message")
-
     def switch_emg_mode(self):
         self.emg_mode = not self.emg_mode
         print("Emergency mode switched to: " + str(self.emg_mode) + " in playback_stm")
@@ -97,16 +61,11 @@ class Player:
 
     def create_machine(self, name):
         t0 = {'source': 'initial', 'target': 'ready'}
-        t1 = {'trigger': 'start', 'source': 'ready', 'target': 'playing'}
-        t2 = {'trigger': 'done', 'source': 'playing', 'target': 'ready'}
-        t8 = {'trigger': 'abort', 'source': 'playing', 'target': 'ready'}
         t5 = {'trigger': 'emg_msg', 'source': 'ready', 'target': 'ready', 'effect': 'switch_emg_mode'}
-        t6 = {'trigger': 'emg_msg', 'source': 'playing', 'target': 'ready', 'effect': 'switch_emg_mode'}
         t7 = {'trigger': 'change_channel_signal', 'source': 'ready', 'target': 'ready', 'effect': 'change_channel'}
         
-        s_playing = {'name': 'playing', 'do': 'play()'}
 
 
-        stm = Machine(name=name, transitions=[t0, t1, t2, t5, t6, t7, t8], states=[s_playing], obj=self)
+        stm = Machine(name=name, transitions=[t0, t5, t7], obj=self)
         self.stm = stm
         return self.stm
