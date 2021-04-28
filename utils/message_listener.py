dc16907cdaf6bc661da23e2cc8341027bf7f27b9
Petter <jgpetter@gmail.com>
2021-04-28 10:39:37 +0200

    relative filepath logging

--- utils/message_listener.py
+++ utils/message_listener.py
@@ -10,33 +10,41 @@ import json
 from time import sleep
 from datetime import datetime
 from datetime import timedelta
+import pathlib
+import os
+
 
 class MessageListener:
     def __init__(self, topic):
 
-        # MQTT settings
+        # MQTT
         self.broker = "mqtt.item.ntnu.no"
         self.port = 1883
         self.topic = topic
+        self.setup_mqtt()
 
-        # Audio settings
+        # Audio
         self.channels = 1
         self.sample_format = pyaudio.paInt16
         self.fs = 44100
         self.chunk = 256
+        self.p = pyaudio.PyAudio()
+
+        # Logging
+        self.log_directory_path = str(pathlib.Path(__file__).parent.absolute()) + "/logfiles/"
+        if not os.path.exists(self.log_directory_path):
+            os.mkdir(self.log_directory_path)
+        self.setup_logging()
 
         # Currently received message
         self.message = {}
 
-        # Setup
-        self.setup_logging()
-        self.setup_mqtt()
-        self.p = pyaudio.PyAudio()
+            
 
     def setup_logging(self):
         logging.basicConfig(format='%(asctime)s %(message)s',
-                            filename= "log_files/" + "Log_" + datetime.now().strftime("%H:%M:%S") + ".txt",
-                            filemode='w',
+                            filename = self.log_directory_path + "Log_" + datetime.now().strftime("%H:%M:%S") + ".txt",
+                            filemode ='w',
                             level=logging.DEBUG)
 
     def setup_mqtt(self):
@@ -76,7 +84,7 @@ class MessageListener:
     # Save audio and return its filename
     def save_audio_msg(self):
         filename = "Audio {}: {}.wav".format(self.message["first_packet_time"], self.message["id"])
-        wf = wave.open("audio_files/input_audio/" + filename, 'wb')
+        wf = wave.open(self.log_directory_path + filename, 'wb')
         wf.setnchannels(self.channels)
         wf.setsampwidth(pyaudio.get_sample_size(self.sample_format))
         wf.setframerate(self.fs)
@@ -87,7 +95,7 @@ class MessageListener:
 
     def log_audio_as_text(self, filename): 
         # Open
-        audio_file = sr.AudioFile("audio_files/input_audio/" + filename)
+        audio_file = sr.AudioFile(self.log_directory_path + filename)
 
         r = sr.Recognizer()
 
@@ -95,6 +103,7 @@ class MessageListener:
             rec_audio = r.record(source)
 
         # Interpret. Casts exeption for unintelligible sounds
+        interpretation = "Error: message not interpreted"
         try:
             interpretation = r.recognize_google(rec_audio)
         except sr.UnknownValueError:
